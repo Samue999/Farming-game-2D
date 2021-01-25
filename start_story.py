2872feca67f95d694a3edecf78ee99ae72e63b53
CreepCrafter07 <65455220+CreepCrafter07@users.noreply.github.com>
2021-01-25 11:34:19 +0100

    Add files via upload

---
+++ start_story.py
@@ -0,0 +1,40 @@
+import time
+
+
+def start():
+    time.sleep(1)
+    print("Farming Game 2D")
+    print("Copyright (c) 2021 Yannick Schlimm")
+    print("Anna Reckers, Tim Grau, Samuel Balog, Ruifeng Du, Maddox Oljaca")
+    print("Licensed under the \"Yannick License\", CC BY-NC-SA")
+    input()
+    print("\n" * 45)
+    print("Once upon a time, there was a boy named Max.")
+    input()
+    print("That boy lived on a big farm with his mother and his father.")
+    input()
+    print("Sometimes, he helped them with the farm work.")
+    input()
+    print("On Friday, the 13. November of 2020, a windstorm came and he flew away.")
+    input()
+    print("HELP! GET ME BACK!, he screamed. But it was too late already.")
+    input()
+    print("Then, everything went black.")
+    input()
+    print("After a few days, two or three maybe, he heard an owl and woke up.")
+    input()
+    print("He was in the middle of a ugly and broken farm.")
+    input()
+    print("What? Where am I? Where are my parents? What is this place?")
+    input()
+    print("You were that boy named Max.")
+    input()
+    print("Now, you are there. You are in the middle of a broken and ugly farm, can't go back and are alone.")
+    input()
+    print("Now, the game starts. You now have to survive on this farm and rebuild it.")
+    input()
+    import game
+    
+
+input("Start with <ENTER>")
+start()
